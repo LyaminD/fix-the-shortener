79c59a0cb3813c290b91fe5e1b21e73719e238ab
LyaminD <Lyamin.diafat@sfr.fr>
2021-09-15 16:37:41 +0200

    Exo finit

--- admin/index.php
+++ admin/index.php
@@ -5,22 +5,41 @@ require '../config.php';
 session_start();
 
 // demande de logout
-
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION);
 }
 
 // tentative de login
-
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
 	$login = $_POST['username'];
-	$hashedPassword = md5($_POST['password']);
 
-	$sql = "select * from users where login = '$login' and password = '$hashedPassword'";
-	$query = $database->query($sql, PDO::FETCH_ASSOC);
-	$users = $query->fetchAll();
+	// mot de passe : version de base (chiffrage md5, insuffisant)
+	$hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+	// mot de passe : version sécurisée (hâchage)
+	// $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+	// version de base
+	 $sql = "select * from users where login = '$login' and password = '$hashedPassword'";
+	 $query = $database->query($sql, PDO::FETCH_ASSOC);
+	 $users = $query->fetchAll();
+
+	// version avec bindparam : entraîne une "login error" en cas d'injection sql
+	// $query = $database->prepare("SELECT login, password FROM users WHERE login = :login AND password = :password");
+	// $query->bindParam(':login', $login, PDO::PARAM_STR);
+	// $query->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
+	// $query->execute();
+	// $users = $query->fetch(PDO::FETCH_ASSOC);
+
+	// version avec requête préparée : idem
+	// $query = $database->prepare("SELECT login, password FROM users WHERE login = :login AND password = :password");
+	// $query->execute(array(
+	// 	"login" => $login,
+	// 	"password" => $hashedPassword
+	// ));
+	// $users = $query->fetch(PDO::FETCH_ASSOC);
 
 	if (isset($users[0])) {
 		$_SESSION['user'] = $users[0];
@@ -31,6 +50,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
 ?>
 <html>
+
 
 <head>
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,200&display=swap" rel="stylesheet">
--- page/api.php
+++ page/api.php
@@ -5,12 +5,15 @@
 $baseUrl = 'http://'.$_SERVER['HTTP_HOST'].'/?q=';
 $code = uniqid();
 
+$filterUrl = htmlspecialchars($_GET['target'], ENT_QUOTES, 'UTF8');
+$filterUrl = filter_var($filterUrl,FILTER_VALIDATE_URL);
+var_dump($filterUrl);
+
 $query = $database->prepare('INSERT INTO urls (code, target) VALUES (:code, :target)');
 $query->execute([
 	':code' => $code,
-	':target' => $_GET['target'],
+	':target' => $filterUrl
 ]);
-
 
 // On retourne une url constituée de l'adresse de base + le code généré
 // en cliquant sur celle-ci, on peut accéder au site
